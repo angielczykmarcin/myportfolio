66e920abf647775c3ef3cd5183b7f42e6e86c305
angielczykmarcin <angielczykmarcin@gmail.com>
2022-12-03 20:02:50 +0100

    Creates maincontenthome component

---
+++ src/components/MainContentHome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainContentHome(){
+    return (
+        <span className="maincontenthome__greetings">
+            Hi, I'm Marcin, welcome to my page! 
+        </span>
+    )
+}
