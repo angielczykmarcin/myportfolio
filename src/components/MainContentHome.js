4d72a777631d7eb73180fc5f7a29b10f852061c3
angielczykmarcin <angielczykmarcin@gmail.com>
2022-12-04 11:11:40 +0100

    adds content to maincontenthome component

--- src/components/MainContentHome.js
+++ src/components/MainContentHome.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function MainContentHome(){
     return (
-        <span className="maincontenthome__greetings">
-            Hi, I'm Marcin, welcome to my page! 
-        </span>
+        <div className='maincontenthome__container'>
+            <span className='maincontenthome__title'>
+                Welcome to my page.
+            </span>
+            <span className="maincontenthome__greetings">
+                I'm Marcin - a software developer with experience in a variety of technologies and tools.
+                Checkout my <span className="maincontenthome__keyword-experience">experience</span> to learn more about me and <span className="maincontenthome__keyword-contact">contact</span> me directly!
+            </span>
+        </div>
     )
 }
