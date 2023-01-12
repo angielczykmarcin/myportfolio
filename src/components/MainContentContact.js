98b95ad3f947ca67d72eb1172d4e12f3579712f4
Marcin Angielczyk <angielczykmarcin@gmail.com>
2023-01-12 18:05:15 +0100

    Introduces spinner and basic validation to the contact form

--- src/components/MainContentContact.js
+++ src/components/MainContentContact.js
@@ -1,41 +1,62 @@
 import React from 'react';
 
 export default function MainContentContact(){
-
+    var Spinner = require('react-spinkit');
     const [showToast, setShowToast] = React.useState(false);
     const [submitDisabled, setSubmitDisabled] = React.useState(false);
-
-   function handleFormSubmit(evt){
-        setSubmitDisabled(previousValue => {
-            return !previousValue;
-        })
-        evt.preventDefault();
-        const formName = document.getElementById('name').value;
-        const formSubject = document.getElementById('subject').value;
-        const formBody = document.getElementById('body').value;
-        fetch('https://marcinangielczyk.pl/contact/',{
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({"name" : formName, "subject": formSubject, "body" : formBody})
-        })
-        .then(response => {
-            if(response.status === 200){
-                clearFormData(formName, formSubject, formBody);
-                setShowToast(previousValue => {
-                    return !previousValue
-                })
-                setTimeout(() => {
-                    setShowToast(previousValue => {
-                        return !previousValue
-                    })
-                }, 2000);
-            }
+    const [errorMessage, setErrorMessage] = React.useState('');
+    
+    function handleFormSubmit(evt){
+            
+       evt.preventDefault();
+       const formName = document.getElementById('name').value;
+       const formSubject = document.getElementById('subject').value;
+       const formBody = document.getElementById('body').value;
+       if(formName && formSubject && formBody){
             setSubmitDisabled(previousValue => {
                 return !previousValue;
             })
-        })
+            fetch('https://marcinangielczyk.pl/contact/',{
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({"name" : formName, "subject": formSubject, "body" : formBody})
+            })
+            .then(response => {
+                if(response.status === 200){
+                    clearFormData(formName, formSubject, formBody);
+                    setErrorMessage( () => {
+                        return 'Message has been sent, thank you!';
+                    })
+                    setShowToast(previousValue => {
+                        return !previousValue
+                    })
+                    setTimeout(() => {
+                        setShowToast(previousValue => {
+                            return !previousValue
+                        })
+                    }, 2000);
+                }
+                setSubmitDisabled(previousValue => {
+                    return !previousValue;
+                })
+            })
+        }
+        else{
+            console.log('else');
+            setErrorMessage( () => {
+                return 'Please fill all form fields before sending the message.';
+            })
+            setShowToast(previousValue => {
+                return !previousValue
+            })
+            setTimeout(() => {
+                setShowToast(previousValue => {
+                    return !previousValue
+                })
+            }, 2000);
+        }
    }
 
    function clearFormData(){
@@ -49,7 +70,7 @@ export default function MainContentContact(){
 
     return (
             <form className="maincontentcontact__form fade-in">
-                
+                {showToast && <div className="maincontentcontact__toast fade-in">{errorMessage}</div>}
                 <label className="maincontentcontact__label">Your name</label>
                 <input type="text" id="name" name="name" className="maincontentcontact__input" placeholder="Your name.."/>
 
@@ -58,9 +79,9 @@ export default function MainContentContact(){
 
                 <label className="maincontentcontact__label">Body</label>
                 <textarea id="body" name="body" className="maincontentcontact__input maincontentcontact__input-body" placeholder="Write something..."></textarea>
-
                 <input className="maincontentcontact__button-submit" type="submit" onClick={handleFormSubmit} value="Submit" disabled={submitDisabled}/>
-                {showToast && <div className="maincontentcontact__toast">Message has been sent, thank you!</div>}
+                {submitDisabled && <Spinner overrideSpinnerClassName="maincontentcontact__spinner" fadeIn="quarter" name='folding-cube' />}
+                
             </form>
     )
 }
